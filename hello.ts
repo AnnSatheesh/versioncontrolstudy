47b6328b146366d9b7b295d13436d22fe04d0ed7
Ann Satheesh <annreshmasatheesh@gmail.com>
2021-10-20 12:37:28 +0530

    First Commit

---
+++ hello.ts
@@ -0,0 +1,13 @@
+class Greeter{
+  greeting: string;
+  constructor(){
+    this.greeting = "World";
+  }
+  greet(){
+    return "Hello, "+
+this.greeting;
+  }
+}
+  
+let greeter = new Greeter();
+console.log(greeter.greet());
